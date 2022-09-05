ac90932bde02b624864ba2e3894c3426354e0038
Madhurjo Das Aishy <madhurjodas@inneed.cloud>
2022-09-05 17:07:05 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <input id="input-number" v-model="inputNumber" type="number" />
+  <h2>{{ result }}</h2>
+</template>
+
+<script>
+import { computed, ref } from "vue";
+export default {
+  setup() {
+    const inputNumber = ref();
+    const result = computed(() => (inputNumber.value >= 50 ? "Pass" : "Fail"));
+
+    return {
+      inputNumber,
+      result,
+    };
+  },
+};
+</script>
